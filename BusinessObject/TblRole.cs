69779cdc6ee0c048364ba705c717debfb8da46b0
Bear-Dev <72665227+babybear0311@users.noreply.github.com>
2021-10-01 20:50:13 +0700

    login form

---
+++ BusinessObject/TblRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataAccess.DataAccess
+{
+    public partial class TblRole
+    {
+        public TblRole()
+        {
+            TblUsers = new HashSet<TblUser>();
+        }
+
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+
+        public virtual ICollection<TblUser> TblUsers { get; set; }
+    }
+}
